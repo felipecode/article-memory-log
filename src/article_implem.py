ea829064352b9984ff9e3cae76b55c5d4ad7610f
felipecode <felipe.alcm@gmail.com>
2020-04-07 17:47:18 -0400

    artiocle imple

--- src/article_implem.py
+++ src/article_implem.py
@@ -4,7 +4,7 @@ import pandas as pd
 import torch
 from torch import nn
 import torchvision
-from torchvision.models import resnet18
+from torchvision.models import resnet18,resnet101
 
 from src.utils.memory import log_mem, log_mem_amp, log_mem_amp_cp, log_mem_cp
 from src.utils.plot import plot_mem, pp
@@ -13,11 +13,11 @@ base_dir = '.'
 # %% Analysis baseline
 os.environ['TORCH_HOME'] = os.path.join('../differentiabledata', '_logs',
                                         'models')
-#model = resnet18().cuda()
-model = torchvision.models.segmentation.deeplabv3_resnet101(pretrained=False).cuda()
+model = resnet101().cuda()
+#model = torchvision.models.segmentation.deeplabv3_resnet101(pretrained=False).cuda()
 
-bs = 128
-input_size = torch.rand(bs, 3, 256, 256).cuda()
+bs = 1
+input_size = torch.rand(bs, 3, 512, 512).cuda()
 
 mem_log = []
 
@@ -29,7 +29,7 @@ except Exception as e:
 df = pd.DataFrame(mem_log)
 
 plot_mem(df, exps=['baseline'],
-         output_file=f'{base_dir}/deeplabv3_resnet101_memory_plot_{bs}_{input_size.shape[2]}.png')
+         output_file=f'{base_dir}/resnet101_memory_plot_{bs}_{input_size.shape[2]}.png')
 
 #pp(df, exp='baseline')
 
