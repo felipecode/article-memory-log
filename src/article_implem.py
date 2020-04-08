4085062f7138f4615e80406ea1067258cc743a27
felipecode <felipe.alcm@gmail.com>
2020-04-07 22:20:27 -0400

    artiocle imple

--- src/article_implem.py
+++ src/article_implem.py
@@ -9,13 +9,43 @@ from torchvision.models import resnet18,resnet101
 from src.utils.memory import log_mem, log_mem_amp, log_mem_amp_cp, log_mem_cp
 from src.utils.plot import plot_mem, pp
 
+class EncoderCompressed(nn.Module):
+    def __init__(self, C=256):
+        super(EncoderCompressed, self).__init__()
+        m = torchvision.models.segmentation.deeplabv3_resnet101(pretrained=True)
+
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=C, kernel_size=5, stride=2, padding=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=C, out_channels=C, kernel_size=5, stride=2, padding=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=C, out_channels=C, kernel_size=5, stride=2, padding=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=C, out_channels=C, kernel_size=5, stride=2, padding=2),
+            nn.Conv2d(256, 512, 3, 1, 1), nn.ReLU(inplace=True),
+            m.backbone.layer2.children()[1:],
+            m.backbone.layer3,
+            m.backbone.layer4,
+            m.classifier,
+            nn.Upsample(scale_factor=16, mode='bilinear', align_corners=True)
+        )
+
+    def forward(self, x, rate=None):
+        return self.model(x)
+
+
+
+
+
+
 base_dir = '.'
 # %% Analysis baseline
 os.environ['TORCH_HOME'] = os.path.join('../differentiabledata', '_logs',
                                         'models')
 #model = resnet101().cuda()
-model = torchvision.models.segmentation.deeplabv3_resnet101(pretrained=True).cuda()
+#model = torchvision.models.segmentation.deeplabv3_resnet101(pretrained=True).cuda()
 
+model = EncoderCompressed()
 bs = 1
 input_size = torch.rand(bs, 3, 512, 512).cuda()
 
@@ -29,7 +59,7 @@ except Exception as e:
 df = pd.DataFrame(mem_log)
 
 plot_mem(df, exps=['baseline'],
-         output_file=f'{base_dir}/deep_lab_resnet101_memory_plot_{bs}_{input_size.shape[2]}.png')
+         output_file=f'{base_dir}/encoder_compressed_memory_plot_{bs}_{input_size.shape[2]}.png')
 
 #pp(df, exp='baseline')
 
