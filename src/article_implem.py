87d1e6c61224089b9378ea43da5634ade2ffaf9d
felipecode <felipe.alcm@gmail.com>
2020-04-08 07:43:07 -0400

    article

--- src/article_implem.py
+++ src/article_implem.py
@@ -23,7 +23,7 @@ class EncoderCompressed(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=C, out_channels=C, kernel_size=5, stride=2, padding=2),
             nn.Conv2d(256, 512, 3, 1, 1), nn.ReLU(inplace=True),
-            m.backbone.layer2.children()[1:],
+            m.backbone.layer2,
             m.backbone.layer3,
             m.backbone.layer4,
             m.classifier,
